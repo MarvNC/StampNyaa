76180646f27c7bb3f460bcce95d7724fef178c47
MarvNC <rotrobmin@gmail.com>
2023-07-03 13:12:03 -0700

    Handle locally added sticker packs

--- src/utils/stickerHandler.js
+++ src/utils/stickerHandler.js
@@ -15,9 +15,20 @@ const getAllStickerPacks = (stickerPacksDir) => {
   }
   const stickerPacks = fs.readdirSync(stickerPacksDir);
   for (const pack of stickerPacks) {
+    // check if info.json exists, if not create it
+    if (!fs.existsSync(path.join(stickerPacksDir, pack, 'info.json'))) {
+      fs.writeFileSync(path.join(stickerPacksDir, pack, 'info.json'), "{}");
+    }
     const stickerPackData = JSON.parse(
       fs.readFileSync(path.join(stickerPacksDir, pack, 'info.json'))
     );
+
+    if (!stickerPackData.title) {
+      stickerPackData.title = pack;
+    }
+    if (!stickerPackData.author) {
+      stickerPackData.author = 'Unknown';
+    }
 
     // pack ID is the folder name
     stickerPackData.id = pack;
