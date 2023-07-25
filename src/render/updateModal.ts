f776ebe4fba636495c634d864c3f0670faf2202a
MarvNC <rotrobmin@gmail.com>
2023-07-24 23:50:33 -0700

    Refactor updatemodal

--- src/render/updateModal.ts
+++ src/render/updateModal.ts
@@ -1,17 +1,17 @@
 const updateModal = {
   setUpUpdateModal: async () => {
     // Update modal
-    const updateModalBackground = document.getElementById('update-background');
-    const updateText = document.getElementById('update-text');
+    const updateModalBackground = document.getElementById('update-background') as HTMLDivElement;
+    const updateText = document.getElementById('update-text') as HTMLHeadingElement;
     async function checkUpdates() {
       const needsUpdateVersion = await api.getUpdates();
       console.log(`Needs update: ${needsUpdateVersion}`);
       if (needsUpdateVersion) {
-        updateModalBackground!.style.display = 'block';
-        updateText!.textContent = `New Update ${needsUpdateVersion} Available!`;
-        const updateButton = document.getElementById('update-button');
-        updateButton!.addEventListener('click', () => {
-          updateModalBackground!.style.display = 'none';
+        updateModalBackground.style.display = 'block';
+        updateText.textContent = `New Update ${needsUpdateVersion} Available!`;
+        const updateButton = document.getElementById('update-button') as HTMLAnchorElement;
+        updateButton.addEventListener('click', () => {
+          updateModalBackground.style.display = 'none';
         });
       }
     }
