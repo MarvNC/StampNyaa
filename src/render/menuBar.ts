da2c99261ddff99dbfd82e17822fe644582c4705
MarvNC <rotrobmin@gmail.com>
2023-07-24 23:43:17 -0700

    Refactor menubar

--- src/render/menuBar.ts
+++ src/render/menuBar.ts
@@ -1,8 +1,8 @@
 const menuBar = {
   setUpMenuBarButtons: async () => {
     // close on X button
-    const closeButton = document.getElementById('close-button');
-    closeButton!.addEventListener('click', () => {
+    const closeButton = document.getElementById('close-button') as HTMLDivElement;
+    closeButton.addEventListener('click', () => {
       api.closeWindow();
     });
   },
